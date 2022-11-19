29c67130acecfa6107b0680d6edfb2ea17807836
Niko <nkfuy@web.de>
2022-11-19 12:50:08 +0100

    eased variable setup

--- buffer.js
+++ buffer.js
@@ -4,17 +4,16 @@ let CONFIG = {
     'POWER_RECORDS': [0, 0, 0],   // Anzahl Werte für Durchschnittsbildung
     'NUMBER_ARRAY_VAL': 2,   // Letzter Index vom Array (0, 1, 2)
     'POWER_THRESHOLD': 9,      // in Watt
-    'SWITCH_STAT': false,    // Zutand des Schalters beim Initialisieren des Skripts
-    'TARGET_URL': "http://192.168.178.59/rpc/"
+    'SWITCH_STAT': false    // Zutand des Schalters beim Initialisieren des Skripts
   };
 
-  let currentPowerMeasurement = 0.0;  // Speichervariable für die aktuelle Leistung in Watt
+  let currentPowerMeasurement = 0.0;  // speichert aktuelle Leistung in Watt
   let itter = 0;      // Itteriert von Null an
 
   Timer.set(CONFIG.CONTROL_TIMER, true, function(ud){
 
-    let avgPower = 0;  // Spechervariable für die durchschnittliche Leistung der letzten Messungen
-    // Funktion zur Messung der aktuellen Leistung in Watt
+    let avgPower = 0;  // Speicherung der aktuellen durchschnittlichen Leistung über die letzten Messungen
+
     Shelly.call(
       "switch.GetStatus", {id: CONFIG.INPUT_ID}, function(result, error_code, error_message, ud)
       {
@@ -30,19 +29,18 @@ let CONFIG = {
       avgPower += CONFIG.POWER_RECORDS[i];
     }
 
-    // Logik zur Auswertung ab wann der Schalter gesetzt wird
     if ((avgPower / (CONFIG.NUMBER_ARRAY_VAL + 1)) > CONFIG.POWER_THRESHOLD && CONFIG.SWITCH_STAT === false){   //
-      CONFIG.SWITCH_STAT = true      // Verbraucher wurde eingeschaltet
-      Shelly.call("Switch.Set", { id: CONFIG.INPUT_ID, on:false },null,null); // dummy bis PV Leistung zur Verfügung steht
-      // print("an", avgPower, CONFIG.POWER_RECORDS[2])
-      Shelly.call("HTTP.GET", { url: CONFIG.TARGET_URL + "Script.Start?id=2" },   // Schalten des Verbrauchers
+      CONFIG.SWITCH_STAT = true      // Schalter einbauen
+      Shelly.call("Switch.Set", { id: CONFIG.INPUT_ID, on:false },null,null);
+      print("an", avgPower, CONFIG.POWER_RECORDS[2])
+      Shelly.call("HTTP.GET", { url: "http://192.168.178.59/rpc/Script.Start?id=2" },
         function (res, error_code, error_msg, ud) {
         if (res.code === 200) {
             print("Successfully transmitted a message");
         };
       },
       null);
-      Shelly.call("HTTP.GET", { url: CONFIG.TARGET_URL + "Script.Stop?id=2" },   // Script zum schalten des Verbrauchers stoppen
+      Shelly.call("HTTP.GET", { url: "http://192.168.178.59/rpc/Script.Stop?id=2" },
         function (res, error_code, error_msg, ud) {
         if (res.code === 200) {
             print("Successfully transmitted a message");
@@ -53,16 +51,16 @@ let CONFIG = {
     else if ((avgPower / (CONFIG.NUMBER_ARRAY_VAL + 1)) < CONFIG.POWER_THRESHOLD && CONFIG.SWITCH_STAT === true){
       CONFIG.SWITCH_STAT = false
 
-      Shelly.call("Switch.Set", { id: CONFIG.INPUT_ID, on:true},null,null);       // Schalten des Verbrauchers
-      //print("aus", avgPower, CONFIG.POWER_RECORDS[2])    // Schalter einbauen
-      Shelly.call("HTTP.GET", { url: CONFIG.TARGET_URL + "Script.Start?id=1" },
+      Shelly.call("Switch.Set", { id: CONFIG.INPUT_ID, on:true},null,null);
+      print("aus", avgPower, CONFIG.POWER_RECORDS[2])    // Schalter einbauen
+      Shelly.call("HTTP.GET", { url: "http://192.168.178.59/rpc/Script.Start?id=1" },
         function (res, error_code, error_msg, ud) {
         if (res.code === 200) {
             print("Successfully transmitted a message");
         };
       },null);
 
-        Shelly.call("HTTP.GET", { url: CONFIG.TARGET_URL + "Script.Stop?id=1" },  // Script zum schalten des Verbrauchers stoppen
+        Shelly.call("HTTP.GET", { url: "http://192.168.178.59/rpc/Script.Stop?id=1" },
         function (res, error_code, error_msg, ud) {
         if (res.code === 200) {
             print("Successfully transmitted a message");
